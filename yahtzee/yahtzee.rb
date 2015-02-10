681b98519d74146c6cc42d8b432fc16531129710
Kasey Bonifacio <kaseybon@gmail.com>
2015-02-10 16:10:17 -0500

    Passing test: adds aces

--- yahtzee/yahtzee.rb
+++ yahtzee/yahtzee.rb
@@ -4,8 +4,21 @@ describe 'Calculate' do
 	it 'adds die' do
 		expect(calculate([1,1,1,1,1])).to eq(5)
 	end
+	it 'adds aces' do
+		expect(calculate([1,1,2,2,3])).to eq(2)
+	end
 end
 
 def calculate(dice)
+	temp = dice.count(1)
+	dice = temp * 1
+end
+
+def chance(dice)
 	dice.reduce(:+)
+end
+
+def aces(dice)
+	temp = dice.count(1)
+	dice = temp * 1
 end
