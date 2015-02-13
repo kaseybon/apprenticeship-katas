cebc1e01d8f9fb304a2fa461c1df3b9f3ab4e637
Kasey Bonifacio <kaseybon@gmail.com>
2015-02-13 16:29:45 -0500

    Passing test: return Fizz when passed 3

--- fizz-buzz/fizz_buzz.rb
+++ fizz-buzz/fizz_buzz.rb
@@ -1,3 +1,15 @@
-def count(value)
-	@value = value
+class FizzBuzz
+	def initialize(value)
+		@value = value
+		@return = @value
+	end
+
+	def count
+		fizz
+		@return
+	end
+
+	def fizz
+		@return = "Fizz" if @value == 3
+	end
 end
--- fizz-buzz/spec/fizz_buzz_spec.rb
+++ fizz-buzz/spec/fizz_buzz_spec.rb
@@ -1,8 +1,13 @@
 require "spec_helper"
 require_relative "../fizz_buzz"
 
-describe 'Fizz Buzz' do
+describe FizzBuzz do
 	it 'return 1 when passed 1' do
-		expect(count(1)).to eq(1)
+		fizzbuzz = FizzBuzz.new 1
+		expect(fizzbuzz.count).to eq(1)
+	end
+	it 'return Fizz when passed 3' do
+		fizzbuzz = FizzBuzz.new 3
+		expect(fizzbuzz.count).to eq('Fizz')
 	end
 end
