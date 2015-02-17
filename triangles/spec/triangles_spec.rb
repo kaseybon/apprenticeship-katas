55416c50b93e7515153b6f384688c204d1ed4445
Kasey Bonifacio <kaseybon@gmail.com>
2015-02-17 16:34:51 -0500

    Refactored

--- triangles/spec/triangles_spec.rb
+++ triangles/spec/triangles_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 require_relative '../triangles'
 
-describe 'Triangles' do
+describe Triangles do
+  let (:triangle) { Triangles.new }
+  subject { triangle }
+
   it 'accepts 3 sides' do
-    expect(sides(3,4,5))
+    expect(subject.sides(3,4,5))
   end
 
   it 'raises an error for sides less than or equal to 0' do
-    expect { sides(0,0,0) }.to raise_error("Not a triangle")
+    expect { subject.sides(0,0,0) }.to raise_error("Not a triangle")
   end
 
   it 'raises an error if the sum of two of the sides are less than or equal than the third' do
-    expect { sides(1,1,3) }.to raise_error("Not a triangle")
+    expect { subject.sides(1,1,3) }.to raise_error("Not a triangle")
   end
 
+  it 'to return equilateral if all sides are equal' do
+    expect(subject.sides(3,3,3)).to eq(:equilateral)
+  end
 end   
--- triangles/triangles.rb
+++ triangles/triangles.rb
@@ -1,7 +1,12 @@
-def sides(a,b,c)
-  triangle = [a,b,c]
-  
-  raise "Not a triangle" if triangle.any? { |x| x <= 0 }
-  raise "Not a triangle" if ((a + b) <= c) || ((a + c) <= b) || ((c + b) <= a)
+class Triangles
+
+   def sides(a,b,c)
+    errors(a,b,c)
+  end
+
+  def errors(a,b,c)
+    triangle = [a,b,c]
+    raise "Not a triangle" if triangle.any? { |x| x <= 0 } || ((a + b) <= c) || ((c + b) <= a)
+  end
 
 end
