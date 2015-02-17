7c8ef86eaad28645836dc229cf071b3f4128a8a9
Kasey Bonifacio <kaseybon@gmail.com>
2015-02-17 15:15:46 -0500

    Passing test: raises error for sides less than or equal to 0

--- triangles/spec/triangles_spec.rb
+++ triangles/spec/triangles_spec.rb
@@ -5,4 +5,10 @@ describe 'Triangles' do
   it 'accepts 3 sides' do
     expect(sides(3,4,5))
   end
+
+  it 'raises an error for sides less than or equal to 0' do
+    expect { sides(0,0,0) }.to raise_error("Not a triangle")
+  end
+
+
 end   
--- triangles/triangles.rb
+++ triangles/triangles.rb
@@ -1,3 +1,5 @@
 def sides(a,b,c)
+  triangle = [a,b,c]
+  raise "Not a triangle" if triangle.any? { |x| x <= 0 }
 
 end
