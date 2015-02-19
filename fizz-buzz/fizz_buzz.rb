357ec3eb0a06547bd3efe05e994d23e9e3b78216
Kasey Bonifacio <kaseybon@gmail.com>
2015-02-19 13:39:40 -0500

    Passing test: Count accepts a value

--- fizz-buzz/fizz_buzz.rb
+++ fizz-buzz/fizz_buzz.rb
@@ -1,18 +1,25 @@
+class Count
+
+  def initialize(value)
+    @count_to = value
+  end
+
+end
+
 class FizzBuzz
+
 	def initialize(value)
 		@value = value
-		@return = @value
-		@fizzybuzzy = { 3 => "Fizz", 5 => "Buzz", 15 => "FizzBuzz" }
 	end
 
-	def count
-		@fizzybuzzy.each do |key, array|
-			check(key, array)
+	def check
+		fizzybuzzy = { 15 => "FizzBuzz", 5 => "Buzz", 3 => "Fizz" }
+
+		fizzybuzzy.each do |key, string|
+		  return string if @value % key == 0
 		end
-		@return
+
+		return
 	end
 
-	def check(num, string)
-		@return = string if @value % num == 0
-	end
 end
--- fizz-buzz/spec/fizz_buzz_spec.rb
+++ fizz-buzz/spec/fizz_buzz_spec.rb
@@ -2,28 +2,43 @@ require "spec_helper"
 require_relative "../fizz_buzz"
 
 describe FizzBuzz do
+
 	it 'return 1 when passed 1' do
 		fizzbuzz = FizzBuzz.new 1
-		expect(fizzbuzz.count).to eq(1)
+		expect(fizzbuzz.check).to eq(1)
 	end
+
 	it 'return Fizz when passed 3' do
 		fizzbuzz = FizzBuzz.new 3
-		expect(fizzbuzz.count).to eq('Fizz')
+		expect(fizzbuzz.check).to eq('Fizz')
 	end
+
 	it 'return Buzz when passed 5' do
 		fizzbuzz = FizzBuzz.new 5
-		expect(fizzbuzz.count).to eq('Buzz')
+		expect(fizzbuzz.check).to eq('Buzz')
 	end
+
 	it 'return Fizz when passed 9' do
 		fizzbuzz = FizzBuzz.new 9
-		expect(fizzbuzz.count).to eq('Fizz')
+		expect(fizzbuzz.check).to eq('Fizz')
 	end
+
 	it 'return Buzz when passed 10' do
 		fizzbuzz = FizzBuzz.new 10
-		expect(fizzbuzz.count).to eq('Buzz')
+		expect(fizzbuzz.check).to eq('Buzz')
 	end
+
 	it 'return FizzBuzz when passed 15' do
 		fizzbuzz = FizzBuzz.new 15
-		expect(fizzbuzz.count).to eq('FizzBuzz')
+		expect(fizzbuzz.check).to eq('FizzBuzz')
 	end
+
+end
+
+describe Count do
+
+  it 'accepts a value' do
+    count = Count.new 1
+  end
+
 end
