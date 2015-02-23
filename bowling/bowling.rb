8147bc4744af399b5223816dc1e9a6386668b51f
Kasey Bonifacio <kaseybon@gmail.com>
2015-02-23 14:02:30 -0500

    Passing test: Bowling adds score

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,19 +1,13 @@
  class Bowling
 
-  def initialize
-    @score = 0
-  end
+  #attr_reader
+
+  # def initialize
+  #   @score = 0
+  # end
 
   def score(first_try, second_try)
-    @score = first_try + second_try
-  end
-
-end
-
-class Frame
-
-  def first_try
-    rand(10)
+    score = first_try + second_try
   end
 
 end
--- bowling/spec/bowling_spec.rb
+++ bowling/spec/bowling_spec.rb
@@ -3,28 +3,9 @@ require_relative '../bowling'
 
 describe Bowling do
   
-  context 'calculates and' do
-
     it 'adds score' do
       bowling = Bowling.new
       expect(bowling.score(3, 5)).to eq(8)
     end
-
-  end
-
-end
-
-describe Frame do
-
-  it 'generates random number greater than or equal to 0 on the first try' do
-    frame = Frame.new
-    expect(frame.first_try).to be >= 0
-  end
-
-  it 'generates random number less than or equal to 10 on the first try' do
-    frame = Frame.new
-    expect(frame.first_try).to be >= 0
-  end
-
 
 end
