e41af5fc858dd3f01eac5858c5453dbeb04e4f5e
Kasey Bonifacio <kaseybon@gmail.com>
2015-03-03 10:55:02 -0500

    Passing test: Player rolls 10 and score equals X

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,7 +1,36 @@
 class Player
   
   def roll(pins)
-    score = pins
+    strike = Strike.new pins
+    regular = Regular.new pins
+
+    [strike, regular].each do |scoring|
+      return scoring.set_score if scoring.equals?
+    end
   end
 
+end
+
+class Score
+  def initialize(pins)
+    @score = pins
+  end
+end
+
+class Strike < Score
+  def equals?
+    true if @score == 10
+  end
+  def set_score
+    @score = 'X'
+  end
+end
+
+class Regular < Score
+  def equals?
+    true
+  end
+  def set_score
+    @score
+  end
 end
--- bowling/spec/bowling_spec.rb
+++ bowling/spec/bowling_spec.rb
@@ -8,4 +8,9 @@ describe Player do
     expect(player.roll(5)).to eq(5)
   end
 
+  it 'rolls 10 and score equals X' do
+    player = Player.new
+    expect(player.roll(10)).to eq('X')
+  end
+
 end
