c1009c9285f61a3c7387647c98a1e591f1fad821
Kasey Bonifacio <kaseybon@gmail.com>
2015-03-03 10:36:17 -0500

    Start Bowling

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,13 +1,3 @@
- class Bowling
-
-  #attr_reader
-
-  # def initialize
-  #   @score = 0
-  # end
-
-  def score(first_try, second_try)
-    score = first_try + second_try
-  end
+class Player
 
 end
--- bowling/spec/bowling_spec.rb
+++ bowling/spec/bowling_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 require_relative '../bowling'
 
-describe Bowling do
+describe Player do
   
-    it 'adds score' do
-      bowling = Bowling.new
-      expect(bowling.score(3, 5)).to eq(8)
-    end
-
 end
