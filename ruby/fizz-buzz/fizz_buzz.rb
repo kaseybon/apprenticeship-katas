d4c6e687dfbaba6ca484009f444edc796978debb
Kasey Bonifacio <kaseybon@gmail.com>
2015-06-18 10:28:19 -0400

    pass: all tests pass

--- ruby/fizz-buzz/fizz_buzz.rb
+++ ruby/fizz-buzz/fizz_buzz.rb
@@ -1,54 +1,11 @@
-def fizzybuzzy(value)
-  fizzbuzz = FizzBuzz.new value
-  fizz = Fizz.new value
-  buzz = Buzz.new value
-  num = Num.new value
-
-  [fizzbuzz, fizz, buzz, num].each do |check|
-    if check.check?
-      return check.output
-    end
-  end
-end
-
-class PassingValues
-  def initialize(value)
-    @value = value
-  end
-end
-
-class FizzBuzz < PassingValues
-	def check?
-    true if @value % 15 == 0
-	end
-  def output
+def fizzybuzzy num
+  if num % 15 == 0
     "FizzBuzz"
-  end
-end
-
-class Fizz < PassingValues
-  def check?
-    true if @value % 3 == 0
-  end
-  def output
+  elsif num % 3 == 0
     "Fizz"
-  end
-end
-
-class Buzz < PassingValues
-  def check?
-    true if @value % 5 == 0
-  end
-  def output
+  elsif num % 5 == 0
     "Buzz"
-  end
-end
-
-class Num < PassingValues
-  def check?
-    true
-  end
-  def output
-    @value
+  else
+    num
   end
 end
